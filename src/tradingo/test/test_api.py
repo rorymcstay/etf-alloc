835921adbf49ed79df3992f2388783dbfaa76f8f
Rory <rory@rorymcstay.com>
2024-08-30 17:11:38 +0100

    Remove return statements

--- src/tradingo/test/test_api.py
+++ src/tradingo/test/test_api.py
@@ -6,18 +6,17 @@ from tradingo.api import Tradingo
 def test_tradingo_api(tradingo: Tradingo):
 
     df = tradingo.prices.close()
-    return df
 
 
 def test_tradingo_api_with(tradingo: Tradingo):
 
     t = tradingo
 
-    return t.instruments.etfs.concat.prices.close.transpose(axis=1).transpose()
+    t.instruments.etfs.concat.prices.close.transpose(axis=1).transpose()
 
 
 def test_tradingo_merge(tradingo: Tradingo):
-    return tradingo.portfolio.model.raw.shares.concat.prices.close(
+    tradingo.portfolio.model.raw.shares.concat.prices.close(
         axis=1, keys=("position", "close")
     )
 
