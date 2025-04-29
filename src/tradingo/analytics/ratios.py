15bd7aa2fdd05c0738b6d48c03c7a89d3e5e2975
Claudio Campanile <claudio.campanile1@gmail.com>
2025-04-29 18:09:38 +0100

    rewrite sharpe to allow for any type of pandas window - including insample - and add constant vol floor

--- src/tradingo/analytics/ratios.py
+++ src/tradingo/analytics/ratios.py
@@ -1,7 +1,12 @@
 """Typical financial ratios."""
 
+from typing import Optional
+
 import numpy as np
 import pandas as pd
+
+from tradingo.analytics.expectation import expectation
+from tradingo.analytics.vol import vol
 
 
 def omega_ratio(returns: pd.Series, required_return: float = 0.0) -> float:
@@ -31,9 +36,46 @@ def omega_ratio(returns: pd.Series, required_return: float = 0.0) -> float:
     return np.nan
 
 
-def sharpe_ratio(returns: pd.Series, required_return: float = 0.0) -> float:
+def sharpe_ratio(
+    returns,
+    annualisation=260,
+    how="ewm",
+    required_return: float = 0.0,
+    volatility_floor: Optional[dict[str, float]] = None,
+    **kwargs,
+):
     """
-    Calculate the Sharpe ratio of a strategy.
+    Calculate the sharpe ratio of a given dataframe of returns.
+
+    :param dataframe: DataFrame containing the data.
+    :param annualisation: The annualisation factor, by default 260.
+    :param how: A DataFrame method which returns a BaseWindow [pandas.core.window.rolling]
+        for calculating covariance: { ewm | rolling | expanding } or 'insample' for the full sample
+    :param required_return: Minimum acceptance return of the investor
+    :param volatility_floor: Minimum acceptance volatiltiy for each market
+    :param **kwargs: keyword arguments
+        Additional arguments for the ewm (exponentially weighted mean) method.
+        For rolling expectation, the window size must be specified in kwargs.
+        For example, `window=10` for a 10-period rolling expectation.
+        For exponential expectation, the halflife can be specified in kwargs.
+        For example, `halflife=10` for a 10-period halflife.
+
+    :returns: The expectation of the data.
     """
-    return_threshold = (1 + required_return) ** (1 / 252) - 1
-    return (returns.mean() * 252 - return_threshold) / (returns.std() * np.sqrt(252))
+
+    exp_returns = expectation(returns, annualisation=annualisation, how=how, **kwargs)
+    volatility = vol(returns, annualisation=annualisation, how=how, **kwargs)
+
+    # TODO: allow for non-constant volfloor, e.g. rolling quantile
+    if volatility_floor is not None:
+        volatility = volatility.apply(
+            lambda col: (
+                col.clip(lower=volatility_floor.get(col.name, 0.0))
+                if col.name in volatility_floor
+                else col
+            )
+        )
+
+    sharpe = (exp_returns - required_return) / volatility
+
+    return sharpe, volatility
